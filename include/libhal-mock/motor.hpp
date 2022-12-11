081922ca2c0260ec928d474de716c748b621e630
MaliaLabor <malia.labor@hotmail.com>
2022-12-10 19:00:11 -0700

    :bricks: Move mocks to libhal-mock
    Move mocks, fakes, simulation and other testing utilities for libhal into
    libhal-mock along with corresponding tests.

---
+++ include/libhal-mock/motor.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "testing.hpp"
+#include <libhal/motor.hpp>
+
+namespace hal::mock {
+/**
+ * @addtogroup motor
+ * @{
+ */
+/**
+ * @brief Mock motor implementation for use in unit tests and simulations with a
+ * spy function for power()
+ *
+ */
+struct motor : public hal::motor
+{
+  /**
+   * @brief Reset spy information for power()
+   *
+   */
+  void reset()
+  {
+    spy_power.reset();
+  }
+
+  /// Spy handler for hal::motor::write()
+  spy_handler<float> spy_power;
+
+private:
+  status driver_power(float p_power) noexcept override
+  {
+    return spy_power.record(p_power);
+  };
+};
+/** @} */
+}  // namespace hal::mock
